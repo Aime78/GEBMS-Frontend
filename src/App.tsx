81b11cdb2dee92cf5e655223a5c4257929e70fed
Rukundo Aime <rukundoaime78@gmail.com>
2024-10-08 15:54:34 +0200

    minor change

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { Button } from "./components/ui/button";
+// import { Button } from "./components/ui/button";
 
 export default function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button>Click me</Button>
+      {/* <Button>Click me</Button> */}
     </div>
   );
 }
